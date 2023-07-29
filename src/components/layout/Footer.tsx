e201e8c5d59ec38c7ddc3f6a7664f2f2065264bb
cowglow <cowglow@gmail.com>
2023-07-29 11:37:28 +0200

    Fix layout

--- src/components/layout/Footer.tsx
+++ src/components/layout/Footer.tsx
@@ -1,17 +1,19 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-const StyledFooter = styled('footer')`
+const StyledFooter = styled("footer")`
   display: flex;
   justify-content: space-between;
   padding: 2rem;
+  color: white;
 `;
 
 export default function Footer() {
-	return (
-		<StyledFooter>
-			<a href='https://github.com/cowglow/react-leaflet' target='_blank'>
-				Git Repo
-			</a>
-		</StyledFooter>
-	);
+  return (
+    <StyledFooter>
+      Footer
+      <a href="https://github.com/cowglow/react-leaflet" target="_blank">
+        Git Repo
+      </a>
+    </StyledFooter>
+  );
 }
--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,31 +1,33 @@
-import { ReactNode } from 'react';
-import styled from 'styled-components';
-import Footer from 'components/layout/Footer.tsx';
-import Header from 'components/layout/Header.tsx';
+import { ReactNode } from "react";
+import styled from "styled-components";
+import Footer from "components/layout/Footer.tsx";
+import Header from "components/layout/Header.tsx";
 
-const StyledWrapper = styled('div')`
+const StyledWrapper = styled("div")`
   display: flex;
   flex-direction: column;
   flex-grow: 1;
   justify-content: center;
+  border: thin solid greenyellow;
 `;
 
 
-const StyledMain = styled('main')`
-  display: block;
+const StyledMain = styled("main")`
+  display: flex;
+  flex-direction: column;
   flex-grow: 1;
 `;
 
 interface MainLayoutProps {
-	children: ReactNode;
+  children: ReactNode;
 }
 
 export default function MainLayout({ children }: MainLayoutProps) {
-	return (
-		<StyledWrapper>
-			<Header />
-			<StyledMain>{children}</StyledMain>
-			<Footer />
-		</StyledWrapper>
-	);
+  return (
+    <StyledWrapper>
+      <Header />
+      <StyledMain>{children}</StyledMain>
+      <Footer />
+    </StyledWrapper>
+  );
 }
