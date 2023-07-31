4416a10166361ef5566f9000c070527035189e94
cowglow <cowglow@gmail.com>
2023-07-31 21:38:32 +0200

    Add polygon workflow

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -9,7 +9,7 @@ import Map from "feature/map/Map.tsx";
 import MapEvents from "feature/map/Map.Events.tsx";
 import { removeFromCollection } from "utils/filters.ts";
 import { useCoordinates } from "hooks/use-coordinates.ts";
-import { Polygon } from "react-leaflet";
+import { LayerGroup, Polygon } from "react-leaflet";
 
 type MarkerType = "marker" | "point"
 type MarkerMode = "single" | "poly"
@@ -32,7 +32,7 @@ export default function App() {
   const [markerType, setMarkerType] = useState<MarkerType>("marker");
   const [markerMode, setMarkerMode] = useState<MarkerMode>("poly");
   const [points, addPoint] = useState<L.LatLng[]>([]);
-  const [polygons, _setPolygons] = useState<L.LatLng[][]>([]);
+  const [polygons, setPolygons] = useState<L.LatLng[][]>([[]]);
 
   const nbgCenter = new L.LatLng(49.4521, 11.0767);
 
@@ -44,8 +44,12 @@ export default function App() {
     if (markerType === "point") {
       addPoint((prevState) => [...prevState, position]);
     }
-    if (editMode === "poly") {
-      console.log("poly", polygons);
+    if (markerMode === "poly") {
+      setPolygons(prevState => {
+        return prevState.map((polygon, index) =>
+          index === prevState.length - 1 ? [...polygon, position] : polygon
+        );
+      });
     }
   };
 
@@ -62,6 +66,22 @@ export default function App() {
     }
 
     data.forEach(item => addPoint(prevState => [...prevState, item]));
+  };
+
+  const initializePoly = () => {
+    if (markerMode === "poly") {
+      setPolygons(prevState => [...prevState, []]);
+    }
+  };
+
+  const toggleMarkerType = (markerType: MarkerType) => {
+    setMarkerType(markerType);
+    initializePoly();
+  };
+
+  const togglePolyCapture = () => {
+    setMarkerMode(prevState => prevState === "single" ? "poly" : "single");
+    initializePoly();
   };
 
   return (
@@ -83,19 +103,24 @@ export default function App() {
       </StyledController>
       <Map position={nbgCenter} zoom={13} scrollWheelZoom={true}>
         <MapEvents onClick={handleMapClick} />
-        {markers &&
-          markers.map((marker, index) => (
-            <MarkerDefault key={index} position={marker} remove={removeMarker} draggable={true} />
-          ))}
-        {points &&
-          points.map((marker, index) => (
-            <MarkerPoint key={index} position={marker} remove={removePoint} draggable={true} />
-          ))}
-        {markers &&
-          [markers].map((markers, index) => (
-            <Polygon key={index} positions={markers} />
-          ))
-        }
+        <LayerGroup>
+          {markers &&
+            markers.map((marker, index) => (
+              <MarkerDefault key={index} position={marker} remove={removeMarker} draggable={true} />))}
+        </LayerGroup>
+        <LayerGroup>
+          {points &&
+            points.map((marker, index) => (
+              <MarkerPoint key={index} position={marker} remove={removePoint} draggable={true} />))}
+        </LayerGroup>
+        <LayerGroup>
+          {polygons &&
+            [polygons].map((points, index) => {
+              if (points.length > 0) {
+                return (<Polygon key={index} positions={points} />);
+              }
+            })}
+        </LayerGroup>
       </Map>
     </MainLayout>
   );
