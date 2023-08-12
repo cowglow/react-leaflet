46160ec1e71ed026aa2c4e24005edcdc3ed49699
cowglow <cowglow@gmail.com>
2023-08-12 07:00:13 +0200

    Prettier

--- src/components/Marker.Default.tsx
+++ src/components/Marker.Default.tsx
@@ -4,16 +4,16 @@ import type { MapMarkerProps } from "feature/map/typing.ts";
 interface MarkerProps extends MapMarkerProps {
 }
 
-export default function MarkerDefault({ children, events, position, remove, alt = '' }: MarkerProps) {
-	return (
-		<MapMarker
-			children={children}
-			events={events}
-			position={position}
-			remove={remove}
-			alt={alt}
-		/>
-	);
+export default function MarkerDefault({ children, events, position, remove, alt = "" }: MarkerProps) {
+  return (
+    <MapMarker
+      children={children}
+      events={events}
+      position={position}
+      remove={remove}
+      alt={alt}
+    />
+  );
 
 
 };
--- src/components/Marker.Point.tsx
+++ src/components/Marker.Point.tsx
@@ -1,9 +1,9 @@
-import MapMarker from 'feature/map/Map.Marker.tsx';
-import { MapMarkerProps } from 'feature/map/typing.ts';
-import L from 'leaflet';
-import pointIcon from 'assets/markers/point/point-icon.png';
-import pointIcon2x from 'assets/markers/point/point-icon-2x.png';
-import pointIconShadow from 'assets/markers/point/point-shadow.png';
+import MapMarker from "feature/map/Map.Marker.tsx";
+import type { MapMarkerProps } from "feature/map/typing.ts";
+import L from "leaflet";
+import pointIcon from "assets/markers/point/point-icon.png";
+import pointIcon2x from "assets/markers/point/point-icon-2x.png";
+import pointIconShadow from "assets/markers/point/point-shadow.png";
 
 
 interface MarkerPointProps extends MapMarkerProps {
@@ -11,25 +11,25 @@ interface MarkerPointProps extends MapMarkerProps {
 
 export default function MarkerPoint({ children, events, position, remove }: MarkerPointProps) {
 
-	const customIcon = L.icon({
-		iconUrl: pointIcon,
-		iconRetinaUrl: pointIcon2x,
-		shadowUrl: pointIconShadow,
-		// iconSize: [60, 55],
-		// shadowSize: [50, 64],
-		// iconAnchor: [22, 94],
-		// shadowAnchor: [4, 62],
-		// popupAnchor: [-3, -76]
-	});
+  const customIcon = L.icon({
+    iconUrl: pointIcon,
+    iconRetinaUrl: pointIcon2x,
+    shadowUrl: pointIconShadow
+    // iconSize: [60, 55],
+    // shadowSize: [50, 64],
+    // iconAnchor: [22, 94],
+    // shadowAnchor: [4, 62],
+    // popupAnchor: [-3, -76]
+  });
 
-	return (
-		<MapMarker
-			children={children}
-			events={events}
-			position={position}
-			remove={remove}
-			icon={customIcon}
-			alt={'MarkerIcon!!!'} />
-	);
+  return (
+    <MapMarker
+      children={children}
+      events={events}
+      position={position}
+      remove={remove}
+      icon={customIcon}
+      alt={"MarkerIcon!!!"} />
+  );
 }
 
