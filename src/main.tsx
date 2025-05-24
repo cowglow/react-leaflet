c9f2a8ad6cd7d6f487d886bc6751e91576aca551
Philip Saa <cowglow@gmail.com>
2025-05-24 08:32:05 +0200

    Dropped Yarn and React-Scan

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,9 @@
-import { scan } from "react-scan"; // must be imported before React and React DOM
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./components/App.tsx";
 import Dialogs from "components/Dialogs.tsx";
 import { ContextProviders } from "./context/context-providers.tsx";
 import "@sakun/system.css";
-
-scan({
-  enabled: import.meta.env.DEV,
-});
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
