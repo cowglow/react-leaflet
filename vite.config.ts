f517598b4ef7b955ed74ce616acc21c88e9a5ca2
Philip Saa <cowglow@gmail.com>
2025-05-26 18:43:41 +0200

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -6,18 +6,19 @@ import * as path from "node:path";
 import * as fs from "node:fs";
 
 // Load self-signed certificates (adjust paths as needed)
+/*
 const certDir = path.resolve(__dirname, "cert");
 const https = {
   key: fs.readFileSync(path.join(certDir, "localhost-key.pem")),
   cert: fs.readFileSync(path.join(certDir, "localhost.pem")),
 };
-
+*/
 // https://vitejs.dev/config/
 export default defineConfig({
   base: "/react-leaflet",
   plugins: [tsconfigPaths(), react(), checker({ typescript: true })],
   server: {
     port: 3000,
-    https: import.meta.env.NODE_MODE === "development" ? test : undefined 
+    /*https: import.meta.env.NODE_MODE === "development" ? test : undefined*/
   },
 });
